648327dce7a68f5664f34887639c702925d1873a
Noppanit Charassinvichai <noppanit.c@gmail.com>
2015-12-21 18:52:55 -0500

    add json

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
 
+from flask import jsonify
 app = Flask(__name__)
-
+app.debug = True
 @app.route('/')
 def hello():
       return 'Hello World!'
+
+@app.route('/process')
+def process():
+      return jsonify(username='fangzhou')
